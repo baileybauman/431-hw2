952298227d2f1ff287c2baf9671f36e696b950b4
Bailey Bauman <baileymbauman@yahoo.com>
2016-02-20 11:43:34 -0600

    test of sorting

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all.order(params[:sort] + ' ' + params[:direction])
+    @movies = Movie.all.order(params[:sort])
   end
 
   def new
