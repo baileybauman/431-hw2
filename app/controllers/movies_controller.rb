411d9c9f7535c053a990cc577110601dc0de7652
Bailey Bauman <baileymbauman@yahoo.com>
2016-02-21 14:58:36 -0600

    testing session

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,12 +14,11 @@ class MoviesController < ApplicationController
     if params[:sort].present?
       @sort_by = params[:sort]
       session[:sort] = @sort_by
-    elsif @sort_by.nil?
-      @sort_by = []
+  
     end
 
-    #if not session[:sort].nil?
-     # @sort_by = session[:sort]
+    if session[:sort].nil? == false
+      @sort_by = session[:sort]
 
     @all_ratings = ['G','PG','PG-13','R']
     @ratings = params[:ratings] || {'G'=>1,'PG'=>1,'PG-13'=>1,'R'=>1}
