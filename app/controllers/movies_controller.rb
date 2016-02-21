f92d53fceaaa898285b8b910e78f6dd7ca1f8fdf
Bailey Bauman <baileymbauman@yahoo.com>
2016-02-21 15:37:01 -0600

    testing session

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,8 +16,7 @@ class MoviesController < ApplicationController
       session[:sort] = @sort_by
     else
       @sort_by = session[:sort]
-      #redirect_to :action=> 'index', :sort => @sort_by
-      #redirect_to 'Release Date', :sort => @sort_by
+      redirect_to :action=> 'index', :sort => @sort_by
     end
 
     if params[:ratings].present?
@@ -26,6 +25,10 @@ class MoviesController < ApplicationController
     elsif !params[:ratings].present? && session[:ratings].nil?
       @ratings = {'G'=>1,'PG'=>1,'PG-13'=>1,'R'=>1}
       session[:ratings] = @ratings
+    elsif !params[:ratings].present? && !params[:sort].present?
+      @ratings = session[:ratings]
+      redirect_to :action=> 'index', :sort => @sort_by
+      redirect_to :action=> 'index', :ratings => @ratings
     else
       @ratings = session[:ratings]
       redirect_to :action=> 'index', :ratings => @ratings
